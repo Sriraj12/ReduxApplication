3f3e0663fbf0907667b091edf66227f1880e976e
Sriraj <srirajappu22@gmail.com>
2022-10-17 15:46:21 +0530

    working on thunk

---
+++ first/src/service/props.js
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import { VALUE } from "./actiontype";
+
+const mapDispatchToProps = (dispatch) =>{
+    return dispatch => {
+    }
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)
