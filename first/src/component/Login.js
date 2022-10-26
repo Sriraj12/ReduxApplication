b2e9845ab491ac2174d0aed7fcdc6727fe8dd35d
sriraj <srirajappu22@gmail.com>
2022-10-26 20:58:19 +0530

    login from json data

--- first/src/component/Login.js
+++ first/src/component/Login.js
@@ -1,46 +1,53 @@
 import React from 'react';
-import { useState } from 'react';
-import {useNavigate} from 'react-router-dom'
-import { Avatar, Grid, Button, TextField, Paper} from '@mui/material';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
+import { Avatar, Grid, Button, TextField, Paper } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { useSelector } from 'react-redux';
 
 
-function Login () {
+function Login() {
     const navigate = useNavigate();
-    // const dispatch = useDispatch();
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch("http://localhost:8000/userdetails")
+            .then((data) => data.json())
+            .then(data => setData(data))
+    }, [])
     const checkuser = useSelector((state) => state.username)
     const checkpass = useSelector((state) => state.password)
-    localStorage.setItem("Token","ticket")
+    localStorage.setItem("Token", "ticket")
 
-    console.log("checkuser",checkuser,"checkpass",checkpass );
-    const [check,setCheck] = useState()
-    const [user,setUser] = useState({
-        username:"",
-        password:"",
+    console.log("checkuser", checkuser, "checkpass", checkpass);
+    const [check, setCheck] = useState()
+    const [user, setUser] = useState({
+        username: "",
+        password: "",
     });
-    console.log("log.user",user.username);
-    console.log("log.pwd",user.password);
-    console.log("value",user);
-    console.log("errorState",check);
-    const handleSubmit =()=>{
+    console.log("log.user", user.username);
+    console.log("log.pwd", user.password);
+    console.log("value", user);
+    console.log("errorState", check);
+    const array1 = data.filter((value)=>
+        (value.username === user.username && value.password === user.password)
+    );
+    const handleSubmit = () => {
         console.log("bksbks");
-        if(user.username === "" && user.password === ""){
+        if (user.username === "" && user.password === "") {
             setCheck(alert('Please enter username and password'))
         }
         else
-        if(user.username === checkuser && user.password === checkpass){
-            console.log("bb2");
-            // localStorage.setItem("Token","ticket")
-            console.log("bb3");
-            setTimeout(()=>{
-                navigate("/home")
-            },5000)
-            
-        }
-        else{
-            setCheck(alert('Please enter valid username and password'))
-        }
+            if (array1.length === 1) {
+                console.log("bb2");
+                console.log("bb3");
+                setTimeout(() => {
+                    navigate("/home")
+                }, 1000)
+
+            }
+            else {
+                setCheck(alert('Please enter valid username and password'))
+            }
     }
     return (
         <Grid className='bgcolor'>
@@ -59,7 +66,7 @@ function Login () {
                         fullWidth
                         value={user.username}
                         autoComplete='off'
-                        onChange={(e) => setUser(prev=>({...prev,username: e.target.value}))}
+                        onChange={(e) => setUser(prev => ({ ...prev, username: e.target.value }))}
                     />
                 </div>
                 <div className="pass">
@@ -73,16 +80,16 @@ function Login () {
                         fullWidth
                         value={user.password}
                         autoComplete='off'
-                        onChange={(e) => setUser(prev=>({...prev, password: e.target.value}))}
+                        onChange={(e) => setUser(prev => ({ ...prev, password: e.target.value }))}
                     />
                 </div>
                 <div className="button1">
-                <Button 
-                variant="contained" 
-                fullWidth 
-                onClick = {handleSubmit}
-                >
-                Sign In</Button>
+                    <Button
+                        variant="contained"
+                        fullWidth
+                        onClick={handleSubmit}
+                    >
+                        Sign In</Button>
                 </div>
                 {/* <div>
                     <Typography>
@@ -93,10 +100,10 @@ function Login () {
                 </div> */}
                 <div>
                     <Button
-                    variant ="contained"
-                    fullWidth
-                    color="inherit"
-                    onClick ={() => {navigate("/signup");}} 
+                        variant="contained"
+                        fullWidth
+                        color="inherit"
+                        onClick={() => { navigate("/signup"); }}
                     >Sign Up</Button>
                 </div>
             </Paper>
